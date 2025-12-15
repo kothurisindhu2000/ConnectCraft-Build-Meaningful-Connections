84a464c79fe859fd4a5ba0d07dcafc1f854586a4
kothurisindhu2000 <kothurisindhu10@gmail.com>
2025-12-15 08:44:26 -0500

    Update UI and improve message preview

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ConnectCraft--Build-Meaningful-Connections/', // MUST match repo name exactly
+  base: '/ConnectCraft-Build-Meaningful-Connections/', // MUST match repo name exactly
   plugins: [react()],
 })
